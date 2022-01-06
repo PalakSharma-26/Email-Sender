b882e7e37928861096fe1de10f1ed921a2439526
Palak <sharmapalak0018@gmail.com>
2022-01-06 18:32:27 +0530

    Added some content

--- index.php
+++ index.php
@@ -13,5 +13,9 @@
     <?php include 'partials/_db.php';?>
     <?php include 'partials/_nav.php';?>
     <!-- <?php echo "Now u can login";?> -->
+    <div style="padding-left:16px">
+        <h2></h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum natus, quaerat eveniet, laudantium necessitatibus, ullam sapiente cum iure qui esse exercitationem. Dignissimos, saepe. Illo perspiciatis vitae expedita natus inventore id, dicta ad, voluptas exercitationem, quidem illum. Quisquam, iure rerum. Eligendi modi consequatur, eius cupiditate nihil hic culpa perferendis aspernatur obcaecati quaerat odit nesciunt assumenda ab magni voluptas iste possimus rem iure cum quis? Laboriosam omnis facilis tenetur ad beatae itaque sunt adipisci quidem libero iste sapiente quasi, repellat blanditiis eum eos, quia eaque saepe alias aperiam. Magni inventore earum culpa voluptatum maiores, eum modi maxime facere neque odit, tempore ratione.<p>
+    </div>
 </body>
 </html>
